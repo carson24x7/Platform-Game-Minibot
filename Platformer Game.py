d9a2284dbf1490e2cc953fff410c16580fe31b85
carson24x7 <84974124+carson24x7@users.noreply.github.com>
2021-05-28 10:16:18 -0400

    Add files via upload

--- Platformer Game.py
+++ Platformer Game.py
@@ -3,6 +3,15 @@ import pygame
 import random
 import json
 
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    application_path = sys._MEIPASS + '/'
+else:
+    application_path = os.path.dirname(__file__) + '/'
+
+
 # Window settings
 GRID_SIZE = 64
 WIDTH = 28 * GRID_SIZE
@@ -33,102 +42,102 @@ WIN = 4
 
 
 # Load fonts
-font_xs = pygame.font.Font(None,14)
-font_xl = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf',96)
-font_lg = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf',64)
-font_md = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf',32)
-font_sm = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf',24)
+font_xs = pygame.font.Font(application_path + 'assets/fonts/Dinomouse-Regular.otf',14)
+font_xl = pygame.font.Font(application_path + 'assets/fonts/Dinomouse-Regular.otf',96)
+font_lg = pygame.font.Font(application_path + 'assets/fonts/Dinomouse-Regular.otf',64)
+font_md = pygame.font.Font(application_path + 'assets/fonts/Dinomouse-Regular.otf',32)
+font_sm = pygame.font.Font(application_path + 'assets/fonts/Dinomouse-Regular.otf',24)
 
 #Load Images
 #Tiles
-plantPurple_img = pygame.image.load('assets/images/tiles/plantPurple.png').convert_alpha()
-cactus_img = pygame.image.load('assets/images/tiles/cactus.png').convert_alpha()
-bush_img = pygame.image.load('assets/images/tiles/bush.png').convert_alpha()
-grass_img = pygame.image.load('assets/images/tiles/grass.png').convert_alpha()
-dirt_img = pygame.image.load('assets/images/tiles/grassdirt.png').convert_alpha()
-greenplatform_img = pygame.image.load('assets/images/tiles/greenblock.png').convert_alpha()
-blueplatform_img = pygame.image.load('assets/images/tiles/blueblock.png').convert_alpha()
-brownplatform_img = pygame.image.load('assets/images/tiles/brownblock.png').convert_alpha()
-greyplatform_img = pygame.image.load('assets/images/tiles/greyblock.png').convert_alpha()
-orangeplatform_img = pygame.image.load('assets/images/tiles/orangeblock.png').convert_alpha()
-spring_img = pygame.image.load('assets/images/tiles/spring.png').convert_alpha()
-pole_img = pygame.image.load('assets/images/tiles/flagpole.png').convert_alpha()
-flagblue_img = pygame.image.load('assets/images/tiles/flagBlue1.png').convert_alpha()
-shroomdirt_img = pygame.image.load('assets/images/tiles/dirt.png').convert_alpha()
-dirtdirt_img = pygame.image.load('assets/images/tiles/dirtdirt.png').convert_alpha()
-mushroombrown_img = pygame.image.load('assets/images/tiles/mushroomBrown.png').convert_alpha()
-mushroomred_img = pygame.image.load('assets/images/tiles/mushroomRed.png').convert_alpha()
-planetdirt_img = pygame.image.load('assets/images/tiles/planetdirt.png').convert_alpha()
-planet_img = pygame.image.load('assets/images/tiles/planet.png').convert_alpha()
-sand_img = pygame.image.load('assets/images/tiles/sand.png').convert_alpha()
-sanddirt_img = pygame.image.load('assets/images/tiles/sanddirt.png').convert_alpha()
-chain_img = pygame.image.load('assets/images/tiles/chain.png').convert_alpha()
+plantPurple_img = pygame.image.load(application_path + 'assets/images/tiles/plantPurple.png').convert_alpha()
+cactus_img = pygame.image.load(application_path + 'assets/images/tiles/cactus.png').convert_alpha()
+bush_img = pygame.image.load(application_path + 'assets/images/tiles/bush.png').convert_alpha()
+grass_img = pygame.image.load(application_path + 'assets/images/tiles/grass.png').convert_alpha()
+dirt_img = pygame.image.load(application_path + 'assets/images/tiles/grassdirt.png').convert_alpha()
+greenplatform_img = pygame.image.load(application_path + 'assets/images/tiles/greenblock.png').convert_alpha()
+blueplatform_img = pygame.image.load(application_path + 'assets/images/tiles/blueblock.png').convert_alpha()
+brownplatform_img = pygame.image.load(application_path + 'assets/images/tiles/brownblock.png').convert_alpha()
+greyplatform_img = pygame.image.load(application_path + 'assets/images/tiles/greyblock.png').convert_alpha()
+orangeplatform_img = pygame.image.load(application_path + 'assets/images/tiles/orangeblock.png').convert_alpha()
+spring_img = pygame.image.load(application_path + 'assets/images/tiles/spring.png').convert_alpha()
+pole_img = pygame.image.load(application_path + 'assets/images/tiles/flagpole.png').convert_alpha()
+flagblue_img = pygame.image.load(application_path + 'assets/images/tiles/flagBlue1.png').convert_alpha()
+shroomdirt_img = pygame.image.load(application_path + 'assets/images/tiles/dirt.png').convert_alpha()
+dirtdirt_img = pygame.image.load(application_path + 'assets/images/tiles/dirtdirt.png').convert_alpha()
+mushroombrown_img = pygame.image.load(application_path + 'assets/images/tiles/mushroomBrown.png').convert_alpha()
+mushroomred_img = pygame.image.load(application_path + 'assets/images/tiles/mushroomRed.png').convert_alpha()
+planetdirt_img = pygame.image.load(application_path + 'assets/images/tiles/planetdirt.png').convert_alpha()
+planet_img = pygame.image.load(application_path + 'assets/images/tiles/planet.png').convert_alpha()
+sand_img = pygame.image.load(application_path + 'assets/images/tiles/sand.png').convert_alpha()
+sanddirt_img = pygame.image.load(application_path + 'assets/images/tiles/sanddirt.png').convert_alpha()
+chain_img = pygame.image.load(application_path + 'assets/images/tiles/chain.png').convert_alpha()
 
 
 #Enemies
-spikes_img = pygame.image.load('assets/images/characters/spikes.png').convert_alpha()
-upside_down_spikes_img =  pygame.image.load('assets/images/characters/spikes_upsidedown.png').convert_alpha()
-barnacle_imgs = [pygame.image.load('assets/images/characters/barnacle.png').convert_alpha(),
-                 pygame.image.load('assets/images/characters/barnacle_attack.png').convert_alpha()]
-bee_imgs_lt =  [pygame.image.load('assets/images/characters/bee.png').convert_alpha(),
-                pygame.image.load('assets/images/characters/bee_move.png').convert_alpha()]
+spikes_img = pygame.image.load(application_path + 'assets/images/characters/spikes.png').convert_alpha()
+upside_down_spikes_img =  pygame.image.load(application_path + 'assets/images/characters/spikes_upsidedown.png').convert_alpha()
+barnacle_imgs = [pygame.image.load(application_path + 'assets/images/characters/barnacle.png').convert_alpha(),
+                 pygame.image.load(application_path + 'assets/images/characters/barnacle_attack.png').convert_alpha()]
+bee_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/bee.png').convert_alpha(),
+                pygame.image.load(application_path + 'assets/images/characters/bee_move.png').convert_alpha()]
 bee_imgs_rt = [pygame.transform.flip(img, True, False)for img in bee_imgs_lt]
-wormGreen_imgs_lt =  [pygame.image.load('assets/images/characters/wormGreen.png').convert_alpha(),
-                      pygame.image.load('assets/images/characters/wormGreen_move.png').convert_alpha()]
+wormGreen_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/wormGreen.png').convert_alpha(),
+                      pygame.image.load(application_path + 'assets/images/characters/wormGreen_move.png').convert_alpha()]
 wormGreen_imgs_rt = [pygame.transform.flip(img, True, False)for img in wormGreen_imgs_lt]
 
-wormPink_imgs_lt =  [pygame.image.load('assets/images/characters/wormPink.png').convert_alpha(),
-                     pygame.image.load('assets/images/characters/wormPink_move.png').convert_alpha()]
+wormPink_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/wormPink.png').convert_alpha(),
+                     pygame.image.load(application_path + 'assets/images/characters/wormPink_move.png').convert_alpha()]
 wormPink_imgs_rt = [pygame.transform.flip(img, True, False)for img in wormPink_imgs_lt]
 
-fly_imgs_lt =  [pygame.image.load('assets/images/characters/fly.png').convert_alpha(),
-                pygame.image.load('assets/images/characters/fly_move.png').convert_alpha()]
+fly_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/fly.png').convert_alpha(),
+                pygame.image.load(application_path + 'assets/images/characters/fly_move.png').convert_alpha()]
 fly_imgs_rt = [pygame.transform.flip(img, True, False)for img in fly_imgs_lt]
 
-saw_imgs_lt =  [pygame.image.load('assets/images/characters/saw.png').convert_alpha(),
-                pygame.image.load('assets/images/characters/saw_move.png').convert_alpha()]
+saw_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/saw.png').convert_alpha(),
+                pygame.image.load(application_path + 'assets/images/characters/saw_move.png').convert_alpha()]
 saw_imgs_rt = [pygame.transform.flip(img, True, False)for img in saw_imgs_lt]
 
-sawhalf_imgs_lt =  [pygame.image.load('assets/images/characters/sawHalf.png').convert_alpha(),
-                    pygame.image.load('assets/images/characters/sawHalf_move.png').convert_alpha()]
+sawhalf_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/sawHalf.png').convert_alpha(),
+                    pygame.image.load(application_path + 'assets/images/characters/sawHalf_move.png').convert_alpha()]
 sawhalf_imgs_rt = [pygame.transform.flip(img, True, False)for img in sawhalf_imgs_lt]
 
-snail_imgs_lt =  [pygame.image.load('assets/images/characters/snail.png').convert_alpha(),
-                  pygame.image.load('assets/images/characters/snail_move.png').convert_alpha()]
+snail_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/snail.png').convert_alpha(),
+                  pygame.image.load(application_path + 'assets/images/characters/snail_move.png').convert_alpha()]
 snail_imgs_rt = [pygame.transform.flip(img, True, False)for img in snail_imgs_lt]
 
-slime_imgs_lt =  [pygame.image.load('assets/images/characters/slimePurple.png').convert_alpha(),
-                  pygame.image.load('assets/images/characters/slimePurple_move.png').convert_alpha()]
+slime_imgs_lt =  [pygame.image.load(application_path + 'assets/images/characters/slimePurple.png').convert_alpha(),
+                  pygame.image.load(application_path + 'assets/images/characters/slimePurple_move.png').convert_alpha()]
 slime_imgs_rt = [pygame.transform.flip(img, True, False)for img in slime_imgs_lt]
 
 #Items
-coinGold_img = pygame.image.load('assets/images/items/coinGold.png').convert_alpha()
+coinGold_img = pygame.image.load(application_path + 'assets/images/items/coinGold.png').convert_alpha()
 
 #Heroes
-hero_idle_imgs_rt = [pygame.image.load('assets/images/characters/alienBlue_stand.png').convert_alpha()]
-hero_walk_imgs_rt = [pygame.image.load('assets/images/characters/alienBlue_walk1.png').convert_alpha(),
-                     pygame.image.load('assets/images/characters/alienBlue_walk2.png').convert_alpha()]
-hero_jump_imgs_rt = [pygame.image.load('assets/images/characters/alienBlue_jump.png').convert_alpha()]
+hero_idle_imgs_rt = [pygame.image.load(application_path +'assets/images/characters/alienBlue_stand.png').convert_alpha()]
+hero_walk_imgs_rt = [pygame.image.load(application_path + 'assets/images/characters/alienBlue_walk1.png').convert_alpha(),
+                     pygame.image.load(application_path + 'assets/images/characters/alienBlue_walk2.png').convert_alpha()]
+hero_jump_imgs_rt = [pygame.image.load(application_path + 'assets/images/characters/alienBlue_jump.png').convert_alpha()]
 hero_idle_imgs_lt = [pygame.transform.flip(img, True, False)for img in hero_idle_imgs_rt]
 hero_walk_imgs_lt = [pygame.transform.flip(img, True, False)for img in hero_walk_imgs_rt]
 hero_jump_imgs_lt = [pygame.transform.flip(img, True, False)for img in hero_jump_imgs_rt]
 
 #Hud
-herohud_img = pygame.image.load('assets/images/hud/hudPlayer_blue.png').convert_alpha()
-fullheart_img = pygame.image.load('assets/images/hud/heart.png').convert_alpha()
-emptyheart_img = pygame.image.load('assets/images/hud/hudHeart_empty.png').convert_alpha()
+herohud_img = pygame.image.load(application_path + 'assets/images/hud/hudPlayer_blue.png').convert_alpha()
+fullheart_img = pygame.image.load(application_path + 'assets/images/hud/heart.png').convert_alpha()
+emptyheart_img = pygame.image.load(application_path + 'assets/images/hud/hudHeart_empty.png').convert_alpha()
 
 # Load sounds
-coin_snd = pygame.mixer.Sound('assets/sounds/collect_point.wav')
-jump_snd = pygame.mixer.Sound('assets/sounds/jump.wav')
-spring_snd = pygame.mixer.Sound('assets/sounds/spring.ogg')
-oof_snd = pygame.mixer.Sound('assets/sounds/oof.ogg')
-goal_snd = pygame.mixer.Sound('assets/sounds/victory.ogg')
+coin_snd = pygame.mixer.Sound(application_path + 'assets/sounds/collect_point.wav')
+jump_snd = pygame.mixer.Sound(application_path + 'assets/sounds/jump.wav')
+spring_snd = pygame.mixer.Sound(application_path + 'assets/sounds/spring.ogg')
+oof_snd = pygame.mixer.Sound(application_path + 'assets/sounds/oof.ogg')
+goal_snd = pygame.mixer.Sound(application_path + 'assets/sounds/victory.ogg')
 
 # Music
-win_music = 'assets/music/winmusic.ogg'
-playing_music = 'assets/music/playingmusic.ogg'
-lose_music = 'assets/music/losemusic.ogg'
+win_music = application_path + 'assets/music/winmusic.ogg'
+playing_music = application_path + 'assets/music/playingmusic.ogg'
+lose_music = application_path + 'assets/music/losemusic.ogg'
 
 #end_music =
 # Levels
